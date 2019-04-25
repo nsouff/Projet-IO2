fe41d258bdcffaf7c3863b3a18698a51637c9786
nsouff <souffan.nathan@gmail.com>
2019-04-25 15:23:56 +0200

    Changement affichage page principal

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style.css">
     <title>NOM DU SITE</title>
   </head>
   <body>
@@ -19,14 +19,20 @@
         <!-- <a href="">Etc...</a> -->
       </nav>
     </header>
-    <form class="" action="index.php" method="post">
-      <input type="text" name="" value="">
-    </form>
-    <div class="annonces">
-      <?php
-        $connex = connex_BD();
-        offres_ville($connex, '1494', 4);
-      ?>
+    <nav class="go_top">
+      <a href="#">^^</a>
+    </nav>
+    <div class="search">
+      <form action="index.php" method="post">
+        <div class="container">
+          <label for="key"><h2>Job</h2></label>
+          <input class="" type="text" name="key" placeholder="Mot clé" id="key">
+        </div>
+        <div class="container">
+          <label for="Où"><h2>Où</h2></label>
+          <input class="Où" type="text" name="Où" placeholder="Ville, département, régions" id="Où">
+        </div>
+      </form>
     </div>
   </body>
 </html>
