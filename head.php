3f000a42d156ff40e61f6e940775caa16ecd9396
nsouff <souffan.nathan@gmail.com>
2019-05-02 19:35:09 +0200

    écriture de la fonction pour la banderole de toutes les pages du site

---
+++ head.php
@@ -0,0 +1,34 @@
+<?php
+  include_once('getResp.php');
+  function head(){
+    $resp = getResp();
+    echo "<header class=\"menu\">";
+    echo "<h1><a href=\"index.php\">NOM DU SITE</a></h1>";
+    echo "<nav class=\"compte\">";
+    if ($resp == 0){
+      echo "<a href=\"inscription.php\">Inscription</a>";
+      echo "<a href=\"page_login.php\">Connexion</a>";
+      echo "<a href=\"inscription_entr.php\">Entrprise? Inscrivez-vous</a>";
+      echo "<a href=\"connexionEntreprise.php\">Connexion entreprise</a>";
+    }
+    else if ($resp < 4){
+      echo "<a href=\"compte.php\">modifez les données</a>";
+      echo "<a href=\"deconnexion.php\">Déconnexion</a>";
+    }
+    else echo "<a href=\"compte_entr.php\">Compte</a>";
+    echo "</nav>";
+    echo "<nav>";
+    if ($resp == 1) {
+
+      echo "<a href=\"search.php\">Trouver un emploie</a>";
+    }
+    if ($resp == 2 || $resp == 3){
+      echo "<a href=\"ApprobationAnnonces.php\">Valider des annonces</a>";
+      echo "<a href=\"ApprobationEntreprise.php\">Valider des entreprises</a>";
+      if ($resp == 3) echo "<a href=\"updateAdmin.php\">Ajoutez/Supprimer des administrateur</a>";
+    }
+    echo "</nav>";
+    echo "</header>";
+
+  }
+ ?>
