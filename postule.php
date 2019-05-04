491031ffd3361c932140b8bbfec1e5c8386b0bcc
nsouff <souffan.nathan@gmail.com>
2019-05-05 01:31:06 +0200

    Explication du cryptage des fichiers enregistré

--- postule.php
+++ postule.php
@@ -1,5 +1,11 @@
 <?php
   include_once('save_link_BD.php');
+  // On veut que les entreprises puissent voir le CV (et possiblement lettre de motivation) des personnes qui ont postulé cependant si l'on donne un nom trop simple au fichier lorsque l'on enregistre celui ci, alors une personne mal intentionnée pourrait facilement récuprérrer ces CV en cherchant quelle pourrait être le nom du fichier.
+
+  // Pour contrer cela on donne un nom compliqué avec md5, seul nous savons comment est crypté le nom fichier il sera donc très difficile pour quelqu'un d'ouvrir un fichier qu'il n'est pas censé ouvrir.
+
+  // Dans .htaccess on rend également index des dossiers innaccessible pour que la liste des CV ou lettre de motivation ne puisse pas être affiché
+
   function postule($connex){
     if ($_FILES['CV']['error'] != 0) echo "des erreurs dans l'upload CV";
     $max_size = 2 * 1024 * 1024;
