755e41503881415abe2f429e75512240ec6b2b3b
nsouff <souffan.nathan@gmail.com>
2019-05-05 11:06:56 +0200

    Correction ortographe error404.pph

--- error404.php
+++ error404.php
@@ -15,7 +15,7 @@
   <body>
     <?php head(); ?>
     <h1>ERREUR 404</h1>
-    <h2>Vous avez dû vous trompez d'url, ne nous trouvons pas la page recherché</h2>
+    <h2>Vous avez dû vous trompez d'url, nous ne trouvons pas la page recherchée</h2>
     <h2>Ne vous inquiétez pas, cela ne se passera pas comme cela pour votre recherche d'emploie! </h2>
     <img src="jobs.jpg" alt="Panneau vers un job">
   </body>
