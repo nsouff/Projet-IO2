523d3f831b756308d370a7f8d7ee954c5dec2eaf
nsouff <souffan.nathan@gmail.com>
2019-05-07 21:05:41 +0200

    rename, et correction pour la conneexion_BD

--- connex_BD.php
+++ connex_BD.php
@@ -1,7 +1,7 @@
 <?php
   //include_once('../code.php');
   function connex_BD(){
-    $connex = mysqli_connect('localhost','admin', 'admin2308', 'Site');
+    $connex = mysqli_connect('localhost','site', 'IO2_site', 'Site');
     if (!$connex) exit;
     mysqli_set_charset($connex, "utf8");
     return $connex;
