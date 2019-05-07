155efa400291a89c8cb226e7d4a59b5352134da4
nsouff <souffan.nathan@gmail.com>
2019-05-07 16:23:35 +0200

    Ecriture du mdp BD directement dans connex_BD.php

--- connex_BD.php
+++ connex_BD.php
@@ -1,7 +1,6 @@
 <?php
-  include_once('../code.php');
   function connex_BD(){
-    $connex = mysqli_connect('localhost','site', getPass(), 'Site');
+    $connex = mysqli_connect('localhost','site', 'IO2_site', 'Site');
     if (!$connex) exit;
     mysqli_set_charset($connex, "utf8");
     return $connex;
